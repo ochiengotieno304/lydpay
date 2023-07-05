dc41fa40fad836225b3be9c61f3d960d69a345bf
Ochieng Otieno <ochiengotieno71@gmail.com>
2023-07-05 15:31:04 +0300

    fix: fix verification bug in webhook verification action

--- app/actions/webhook/verification.rb
+++ app/actions/webhook/verification.rb
@@ -11,17 +11,15 @@ module Wapay
           required(:"hub.challenge").filled(:string)
         end
 
-
-
         def handle(request, response)
-          token = "TOKEN"
+          token = ENV['VERIFY_TOKEN']
+
           halt 422 unless request.params.valid?
           mode = request.params[:"hub.mode"]
           verify_token = request.params[:"hub.verify_token"]
           challenge = request.params[:"hub.challenge"]
-          puts("#{verify_token == token}")
-          puts("#{token} #{verify_token}")
-          # halt 403 unless mode === "subscribe" and verify_token === @token
+
+          halt 403 unless mode === "subscribe" and verify_token.eql? token
 
           response.status = 200
           response.body = challenge.to_i
