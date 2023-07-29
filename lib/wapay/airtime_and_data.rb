103a882c508f11e66204941d5fcb0bf0df37b3a2
Ochieng Otieno <ochiengotieno71@gmail.com>
2023-07-29 13:16:03 +0300

    update airtime top up variables

--- lib/wapay/airtime_and_data.rb
+++ lib/wapay/airtime_and_data.rb
@@ -6,15 +6,15 @@ require 'ostruct'
 module Wapay
   class AirtimeAndData
     @username = ENV['AT_USERNAME']
-    @api_key = ENV['AT_API_KEY']
-    @endpoint = ENV['AT_ENDPOINT']
+    @api_key = ENV['AT_API_KEY_LIVE']
+    @endpoint = ENV['AT_ENDPOINT_LIVE']
 
     def self.send_airtime(recipient, amount)
       from_account_balance = User.user_data(recipient).balance.to_i
 
       if from_account_balance > amount.to_i
         int_recipient = recipient[1..].rjust(13, '+254')
-        data = { 'username' => 'sandbox',
+        data = { 'username' => @username,
                  'recipients' => "[{\"phoneNumber\": \"#{int_recipient}\",\"amount\": \"KES #{amount}\" }]" }
         response = connection.post('/version1/airtime/send') do |req|
           req.headers['apiKey'] = @api_key
